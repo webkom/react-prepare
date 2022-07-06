03e94c9c184d0b92f84c837ecf748bbf8181822b
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-07-06 18:38:42 +0200

    add types to isThenable and add check for "catch" function
    The only place we are ever using isThenable (in prepareCompositeElement in prepare.ts) we are also assuming that any thenable value also has a catch method

--- src/tests/isThenable.spec.js
+++ src/tests/isThenable.spec.js
@@ -28,7 +28,15 @@ describe('isThenable(p)', () => {
   it('recognizes custom thenable as thenable', () => {
     const thenable = {
       then: () => Promise.resolve(),
+      catch: () => Promise.resolve(),
     };
     assert(isThenable(thenable));
+  });
+
+  it('recognizes custom thenable missing catch function as non-thenable', () => {
+    const thenable = {
+      then: () => Promise.resolve(),
+    };
+    assert(!isThenable(thenable));
   });
 });
--- src/utils/isThenable.ts
+++ src/utils/isThenable.ts
@@ -1,5 +1,10 @@
-function isThenable(p) {
-  return p && typeof p === 'object' && typeof p.then === 'function';
-}
+// The typing here is not 100% accurate, as there are no guarantees that the
+// "then" and "catch" methods have the same signature as a native promise.
+// However, it is good enough for any usage in this package.
+const isThenable = (p: unknown): p is Promise<unknown> =>
+  !!p &&
+  typeof p === 'object' &&
+  typeof (p as Partial<Promise<unknown>>).then === 'function' &&
+  typeof (p as Partial<Promise<unknown>>).catch === 'function';
 
 export default isThenable;
